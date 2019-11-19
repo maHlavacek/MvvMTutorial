4e970d0caf94afbe7aaa4b76fdbd13f34990e118
maHlavacek <martin_hlavacek@icloud.com>
2019-11-19 18:48:57 +0100

    ViewModels added

---
+++ MvvMTutorial.WpfApp/ViewModel/EmployeeViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MvvMTutorial.WpfApp.ViewModel
+{
+    public class EmployeeViewModel : BaseViewModel
+    {
+
+    }
+}
