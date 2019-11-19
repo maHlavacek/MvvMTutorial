25af02d561c10307dd470e378e1132bc8a725777
maHlavacek <martin_hlavacek@icloud.com>
2019-11-19 19:35:34 +0100

    RelayCommand implemented

---
+++ MvvMTutorial.WpfApp/Command/RelayCommand.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace MvvMTutorial.WpfApp.Command
+{
+    class RelayCommand : ICommand
+    {
+        private readonly Action<Object> execute;
+        private readonly Predicate<Object> canExecute;
+
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        public RelayCommand(Action<Object> execute, Predicate<Object> canExecute)
+        {
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return canExecute != null ? canExecute(parameter) : false;
+        }
+
+        public void Execute(object parameter)
+        {
+            if (execute != null) execute(parameter);
+        }
+    }
+}
--- MvvMTutorial.WpfApp/ViewModel/EmployeeViewModel.cs
+++ MvvMTutorial.WpfApp/ViewModel/EmployeeViewModel.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
 using System.Text;
+using System.Windows.Input;
 
 namespace MvvMTutorial.WpfApp.ViewModel
 {
@@ -10,6 +12,54 @@ namespace MvvMTutorial.WpfApp.ViewModel
         private Logic.Entities.Employee selectedEmployee = null;
 
         private ObservableCollection<Logic.Entities.Employee> employees;
+
+        public EmployeeViewModel()
+        {
+            LoadEmployees();
+        }
+
+        private void LoadEmployees()
+        {
+            if (employees == null)
+            {
+                employees = new ObservableCollection<Logic.Entities.Employee>();
+            }
+
+            foreach (var item in Logic.EmployeeRepository.GetEmployees())
+            {
+                employees.Add(item);
+            }
+
+            if (employees.Any())
+            {
+                selectedEmployee = employees[0];
+            }
+            else
+            {
+                selectedEmployee = new Logic.Entities.Employee();
+            }
+        }
+
+        public string FirstName
+        {
+            get { return SelectedEmployee.FirstName; }
+            set
+            {
+                SelectedEmployee.FirstName = value;
+                OnPropertyChanged(nameof(FirstName));
+            }
+        }
+
+        public string LastName
+        {
+            get { return SelectedEmployee.LastName; }
+            set
+            {
+                SelectedEmployee.LastName = value;
+                OnPropertyChanged(nameof(LastName));
+            }
+        }
+
 
         public Logic.Entities.Employee SelectedEmployee
         {
@@ -28,6 +78,18 @@ namespace MvvMTutorial.WpfApp.ViewModel
             {
                 employees = value;
                 OnPropertyChanged(nameof(Employees));
+            }
+        }
+
+        private ICommand cmdSaveChanges;
+        public ICommand CmdSaveChanges
+        {
+            get
+            {
+                if(cmdSaveChanges == null)
+                {
+                    cmdSaveChanges = new RelayCommand();
+                }
             }
         }
 
