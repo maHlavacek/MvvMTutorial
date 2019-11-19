b795edc8f67f89acb6e27ab428d2ccee94aa870a
maHlavacek <martin_hlavacek@icloud.com>
2019-11-19 19:12:10 +0100

    Properties defined

--- MvvMTutorial.WpfApp/ViewModel/EmployeeViewModel.cs
+++ MvvMTutorial.WpfApp/ViewModel/EmployeeViewModel.cs
@@ -7,7 +7,19 @@ namespace MvvMTutorial.WpfApp.ViewModel
 {
     public class EmployeeViewModel : BaseViewModel
     {
+        private Logic.Entities.Employee selectedEmployee = null;
+
         private ObservableCollection<Logic.Entities.Employee> employees;
+
+        public Logic.Entities.Employee SelectedEmployee
+        {
+            get { return selectedEmployee; }
+            set 
+            { 
+                selectedEmployee = value;
+                OnPropertyChanged(nameof(SelectedEmployee));
+            }
+        }
 
         public ObservableCollection<Logic.Entities.Employee> Employees 
         { 
