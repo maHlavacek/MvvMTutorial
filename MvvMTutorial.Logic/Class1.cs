f61add4e05b6cdbde7a5a6681034b204aacce00d
maHlavacek <martin_hlavacek@icloud.com>
2019-11-19 19:08:40 +0100

    ViewModels added

--- MvvMTutorial.Logic/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace MvvMTutorial.Logic
-{
-    public class Class1
-    {
-    }
-}
--- MvvMTutorial.WpfApp/ViewModel/EmployeeViewModel.cs
+++ MvvMTutorial.WpfApp/ViewModel/EmployeeViewModel.cs
@@ -1,11 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
 
 namespace MvvMTutorial.WpfApp.ViewModel
 {
     public class EmployeeViewModel : BaseViewModel
     {
+        private ObservableCollection<Logic.Entities.Employee> employees;
+
+        public ObservableCollection<Logic.Entities.Employee> Employees 
+        { 
+            get { return employees; }
+            set
+            {
+                employees = value;
+                OnPropertyChanged(nameof(Employees));
+            }
+        }
 
     }
 }
