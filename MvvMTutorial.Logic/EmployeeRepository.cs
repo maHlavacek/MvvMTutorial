84033aab8eebdcf5153b091c95fadf51e4c9a574
maHlavacek <martin_hlavacek@icloud.com>
2019-11-19 18:44:28 +0100

    Repository implemented

---
+++ MvvMTutorial.Logic/EmployeeRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MvvMTutorial.Logic
+{
+    public class EmployeeRepository
+    {
+        private static List<Entities.Employee> employees;
+
+        static EmployeeRepository()
+        {
+            employees = new List<Entities.Employee>()
+            {
+                new Entities.Employee {LastName = "Müller", FirstName = "Zeki"},
+                new Entities.Employee {LastName = "Seidl", FirstName = "Martin"},
+                new Entities.Employee {LastName = "Hofer", FirstName = "Florian"}
+            };
+        }
+
+        public static Entities.Employee[] GetEmployees()
+        {
+            return employees.ToArray();
+        }
+
+        public static void Add(string lastName, string firstName)
+        {
+            employees.Add(new Entities.Employee { FirstName = firstName, LastName = lastName });
+        }
+    }
+}
