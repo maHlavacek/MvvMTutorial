501e29714a90d28f568605bb519476ac859c5bf0
maHlavacek <martin_hlavacek@icloud.com>
2019-11-19 18:38:59 +0100

    add employee

---
+++ MvvMTutorial.Logic/Entities/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MvvMTutorial.Logic.Entities
+{
+    public class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName => $"{LastName} {FirstName}";
+    }
+}
